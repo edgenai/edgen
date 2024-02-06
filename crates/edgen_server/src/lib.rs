a0e58434ae4c3d53bc3dfaf615980677a79b5331
Tobias Schoofs <tobias.schoofs@gmx.net>
2024-02-06 12:11:03 +0000

    [feat/issue10] audio transcriptions status added

--- crates/edgen_server/src/lib.rs
+++ crates/edgen_server/src/lib.rs
@@ -186,6 +186,11 @@ async fn run_server(args: &cli::Serve) -> bool {
             "/v1/chat/completions/status",
             axum::routing::get(status::chat_completions_status),
         )
+        // ---- Audio ----------------------------------------------------------
+        .route(
+            "/v1/audio/transcriptions/status",
+            axum::routing::get(status::audio_transcriptions_status),
+        )
         // -- Miscellaneous services -------------------------------------------
         .route("/v1/misc/version", axum::routing::get(misc::edgen_version))
         .layer(CorsLayer::permissive());
--- crates/edgen_server/src/status.rs
+++ crates/edgen_server/src/status.rs
@@ -30,9 +30,17 @@ use utoipa::ToSchema;
 /// The status is returned as json value AIStatus.
 /// For any error, the version endpoint returns "internal server error".
 pub async fn chat_completions_status() -> Response {
-    let rwstate = get_chat_completions_status();
-    let locked = rwstate.read().await;
-    Json(locked.clone()).into_response()
+    let state = get_chat_completions_status().read().await;
+    Json(state.clone()).into_response()
+}
+
+/// GET `/v1/audio/transcriptions/status`: returns the current status of the /audio/transcriptions endpoint.
+///
+/// The status is returned as json value AIStatus.
+/// For any error, the version endpoint returns "internal server error".
+pub async fn audio_transcriptions_status() -> Response {
+    let state = get_audio_transcriptions_status().read().await;
+    Json(state.clone()).into_response()
 }
 
 /// Recent Activity on a specific endpoint, e.g. Completions or Download.
@@ -85,88 +93,146 @@ impl Default for AIStatus {
     }
 }
 
-const MAX_ERRORS: usize = 32;
-
-/// Get a protected chat completions status.
-/// Call read() or write() on the returned value to get either read or write access.
-pub fn get_chat_completions_status() -> &'static RwLock<AIStatus> {
-    &AISTATES.endpoints[EP_CHAT_COMPLETIONS]
-}
-
-/// Get a protected chat completions status.
-/// Call read() or write() on the returned value to get either read or write access.
-pub async fn reset_chat_completions_status() {
-    let mut status = get_chat_completions_status().write().await;
-    let default = AIStatus::default();
-    status.active_model = default.active_model;
-    status.last_activity = default.last_activity;
-    status.last_activity_result = default.last_activity_result;
-    status.completions_ongoing = default.completions_ongoing;
-    status.download_ongoing = default.download_ongoing;
-    status.download_progress = default.download_progress;
-    status.last_errors = VecDeque::from([]);
-}
-
-/// Get a protected audio transcriptions status.
-/// Call read() or write() on the returned value to get either read or write access.
-pub fn get_audio_transcriptions_status() -> &'static RwLock<AIStatus> {
-    &AISTATES.endpoints[EP_AUDIO_TRANSCRIPTIONS]
-}
-
-/// Set active model
-pub async fn set_chat_completions_active_model(model: &str) {
-    let rwstate = get_chat_completions_status();
-    let mut state = rwstate.write().await;
-    state.active_model = model.to_string();
-}
-
-/// Set download ongoing
-pub async fn set_chat_completions_download(ongoing: bool) {
-    if ongoing {
-        info!("starting model download");
-    } else {
-        info!("model download finished");
-    };
-    let rwstate = get_chat_completions_status();
-    let mut state = rwstate.write().await;
-    state.download_ongoing = ongoing;
-}
-
-/// Set download progress
-pub async fn set_chat_completions_progress(progress: u64) {
-    let rwstate = get_chat_completions_status();
-    let mut state = rwstate.write().await;
-    state.download_progress = progress;
-}
-
-/// Observe download progress
-pub async fn observe_chat_completions_progress(
-    datadir: &PathBuf,
-    size: Option<u64>,
-    download: bool,
-) -> tokio::task::JoinHandle<()> {
-    observe_progress(datadir, size, download).await
-}
-
-/// Add an error to the last errors
-pub async fn add_chat_completions_error<E>(e: E)
-where
-    E: Error,
-{
-    let rwstate = get_chat_completions_status();
-    let mut state = rwstate.write().await;
-    if state.last_errors.len() > MAX_ERRORS {
-        state.last_errors.pop_front();
-    }
-    state.last_errors.push_back(format!("{:?}", e));
-}
-
 // axum provides shared state but using this shared state would force us
 // to pass the state on to all function that may change the state.
 static AISTATES: Lazy<AIStates> = Lazy::new(Default::default);
 
 const EP_CHAT_COMPLETIONS: usize = 0;
 const EP_AUDIO_TRANSCRIPTIONS: usize = 1;
+
+const MAX_ERRORS: usize = 32;
+
+/// Get a protected chat completions status.
+/// Call read() or write() on the returned value to get either read or write access.
+pub fn get_chat_completions_status() -> &'static RwLock<AIStatus> {
+    get_status(EP_CHAT_COMPLETIONS)
+}
+
+/// Get a protected audio transcriptions status.
+/// Call read() or write() on the returned value to get either read or write access.
+pub fn get_audio_transcriptions_status() -> &'static RwLock<AIStatus> {
+    get_status(EP_AUDIO_TRANSCRIPTIONS)
+}
+
+fn get_status(idx: usize) -> &'static RwLock<AIStatus> {
+    &AISTATES.endpoints[idx]
+}
+
+/// Reset the chat completions status to its defaults
+pub async fn reset_chat_completions_status() {
+    reset_status(EP_CHAT_COMPLETIONS).await;
+}
+
+/// Reset the audio transcriptions status to its defaults
+pub async fn reset_audio_transcriptions_status() {
+    reset_status(EP_AUDIO_TRANSCRIPTIONS).await;
+}
+
+async fn reset_status(idx: usize) {
+    let mut status = get_status(idx).write().await;
+    *status = AIStatus::default();
+}
+
+/// Set chat completions active model
+pub async fn set_chat_completions_active_model(model: &str) {
+    set_active_model(EP_CHAT_COMPLETIONS, model).await;
+}
+
+/// Set audio transcriptions active model
+pub async fn set_audio_transcriptions_active_model(model: &str) {
+    set_active_model(EP_AUDIO_TRANSCRIPTIONS, model).await;
+}
+
+async fn set_active_model(idx: usize, model: &str) {
+    let mut state = get_status(idx).write().await;
+    state.active_model = model.to_string();
+}
+
+/// Set chat completions download ongoing
+pub async fn set_chat_completions_download(ongoing: bool) {
+    if ongoing {
+        info!("starting chat completions model download");
+    } else {
+        info!("chat completions model download finished");
+    };
+    set_download(EP_CHAT_COMPLETIONS, ongoing).await;
+}
+
+/// Set audio transcriptions download ongoing
+pub async fn set_audio_transcriptions_download(ongoing: bool) {
+    if ongoing {
+        info!("starting chat completions model download");
+    } else {
+        info!("chat completions model download finished");
+    };
+    set_download(EP_AUDIO_TRANSCRIPTIONS, ongoing).await;
+}
+
+async fn set_download(idx: usize, ongoing: bool) {
+    let mut state = get_status(idx).write().await;
+    state.download_ongoing = ongoing;
+}
+
+/// Set chat completions download progress
+pub async fn set_chat_completions_progress(progress: u64) {
+    set_progress(EP_CHAT_COMPLETIONS, progress).await;
+}
+
+/// Set audio transcriptions download progress
+pub async fn set_audio_transcriptions_progress(progress: u64) {
+    set_progress(EP_AUDIO_TRANSCRIPTIONS, progress).await;
+}
+
+async fn set_progress(idx: usize, progress: u64) {
+    let mut state = get_status(idx).write().await;
+    state.download_progress = progress;
+}
+
+/// Observe chat completions download progress
+pub async fn observe_chat_completions_progress(
+    datadir: &PathBuf,
+    size: Option<u64>,
+    download: bool,
+) -> tokio::task::JoinHandle<()> {
+    observe_progress(EP_CHAT_COMPLETIONS, datadir, size, download).await
+}
+
+/// Observe audio transcptions download progress
+pub async fn observe_audio_transcriptions_progress(
+    datadir: &PathBuf,
+    size: Option<u64>,
+    download: bool,
+) -> tokio::task::JoinHandle<()> {
+    observe_progress(EP_AUDIO_TRANSCRIPTIONS, datadir, size, download).await
+}
+
+/// Add an error to the last errors in chat completions
+pub async fn add_chat_completions_error<E>(e: E)
+where
+    E: Error,
+{
+    add_error(EP_CHAT_COMPLETIONS, e).await;
+}
+
+/// Add an error to the last errors in audio transcriptions
+pub async fn add_audio_transcriptions_error<E>(e: E)
+where
+    E: Error,
+{
+    add_error(EP_AUDIO_TRANSCRIPTIONS, e).await;
+}
+
+async fn add_error<E>(idx: usize, e: E)
+where
+    E: Error,
+{
+    let rwstate = get_status(idx);
+    let mut state = rwstate.write().await;
+    if state.last_errors.len() > MAX_ERRORS {
+        state.last_errors.pop_front();
+    }
+    state.last_errors.push_back(format!("{:?}", e));
+}
 
 struct AIStates {
     endpoints: Vec<RwLock<AIStatus>>,
@@ -199,6 +265,7 @@ fn internal_server_error(msg: &str) -> Response {
 // - until the tempfile disappears or no progress was made for 1 minute.
 // TODO: This code should go to the module manager.
 async fn observe_progress(
+    idx: usize,
     datadir: &PathBuf,
     size: Option<u64>,
     download: bool,
@@ -217,11 +284,11 @@ async fn observe_progress(
         }
         let size = size.unwrap();
 
-        if !have_tempdir(&tmp).await {
+        if !have_tempdir(idx, &tmp).await {
             return;
         }
 
-        let t = wait_for_tempfile(&tmp).await;
+        let t = wait_for_tempfile(idx, &tmp).await;
         if t.is_none() {
             return;
         }
@@ -244,7 +311,7 @@ async fn observe_progress(
                 return;
             };
 
-            set_chat_completions_progress(p).await;
+            set_progress(idx, p).await;
             tokio::time::sleep(std::time::Duration::from_millis(500)).await;
             m = tokio::fs::metadata(&f.path()).await;
         }
@@ -253,7 +320,7 @@ async fn observe_progress(
     progress_handle
 }
 
-async fn have_tempdir(tmp: &PathBuf) -> bool {
+async fn have_tempdir(idx: usize, tmp: &PathBuf) -> bool {
     let mut d = tokio::fs::metadata(&tmp).await;
     for _ in 0..10 {
         if d.is_ok() {
@@ -267,7 +334,7 @@ async fn have_tempdir(tmp: &PathBuf) -> bool {
             "progress observer: can't read tmp directory ({:?}). Giving up",
             d
         );
-        add_chat_completions_error(d.unwrap_err()).await;
+        add_error(idx, d.unwrap_err()).await;
         return false;
     };
 
@@ -276,7 +343,7 @@ async fn have_tempdir(tmp: &PathBuf) -> bool {
 
 // TODO: we use the first file we find in the tmp directory.
 //       we should instead *know* the name of the file.
-async fn wait_for_tempfile(tmp: &PathBuf) -> Option<std::fs::DirEntry> {
+async fn wait_for_tempfile(idx: usize, tmp: &PathBuf) -> Option<std::fs::DirEntry> {
     for _ in 0..30 {
         tokio::time::sleep(std::time::Duration::from_millis(100)).await;
         let es = std::fs::read_dir(&tmp);
@@ -285,7 +352,7 @@ async fn wait_for_tempfile(tmp: &PathBuf) -> Option<std::fs::DirEntry> {
                 "progress observer: cannot read tmp directory ({:?}). Giving up",
                 es
             );
-            add_chat_completions_error(es.unwrap_err()).await;
+            add_error(idx, es.unwrap_err()).await;
             return None;
         };
         for e in es.unwrap() {
@@ -339,7 +406,7 @@ mod tests {
     // The problem is that tests run in parallel
     // and we are testing one common resource, the global status.
     #[tokio::test]
-    async fn test_get_status() {
+    async fn test_chat_completions_status() {
         reset_chat_completions_status().await;
 
         // default
@@ -451,6 +518,128 @@ mod tests {
         set_chat_completions_active_model(&model).await;
 
         let response = server.get("/v1/chat/completions/status").await;
+
+        response.assert_status_ok();
+        assert!(response.text().len() > 0);
+        assert_eq!(response.json::<AIStatus>().active_model, model);
+    }
+
+    // This test should not be split into sub-tests.
+    // The problem is that tests run in parallel
+    // and we are testing one common resource, the global status.
+    #[tokio::test]
+    async fn test_audio_transcriptions_status() {
+        reset_audio_transcriptions_status().await;
+
+        // default
+        let mut expected = AIStatus::default();
+
+        {
+            let status = get_audio_transcriptions_status().read().await;
+            assert_eq!(*status, AIStatus::default());
+        }
+
+        // download ongoing
+        expected.download_ongoing = true;
+        set_audio_transcriptions_download(true).await;
+
+        {
+            let status = get_audio_transcriptions_status().read().await;
+            assert_eq!(*status, expected);
+        }
+
+        // download progress
+        expected.download_progress = 42;
+        set_audio_transcriptions_progress(42).await;
+
+        {
+            let status = get_audio_transcriptions_status().read().await;
+            assert_eq!(*status, expected);
+        }
+
+        // errors
+        let e1 = Error::new(ErrorKind::Interrupted, "couldn't finish");
+        expected.last_errors.push_back(format!("{:?}", e1));
+        add_audio_transcriptions_error(e1).await;
+
+        {
+            let status = get_audio_transcriptions_status().read().await;
+            assert_eq!(*status, expected);
+        }
+
+        let e2 = Error::new(ErrorKind::NotFound, "I still haven't found");
+        expected.last_errors.push_back(format!("{:?}", e2));
+        add_audio_transcriptions_error(e2).await;
+
+        assert_eq!(expected.last_errors.len(), 2);
+
+        {
+            let status = get_audio_transcriptions_status().read().await;
+            assert_eq!(*status, expected);
+        }
+
+        let e3 = Error::new(ErrorKind::PermissionDenied, "verboten");
+        expected.last_errors.push_back(format!("{:?}", e3));
+        add_audio_transcriptions_error(e3).await;
+
+        assert_eq!(expected.last_errors.len(), 3);
+
+        {
+            let status = get_audio_transcriptions_status().read().await;
+            assert_eq!(*status, expected);
+        }
+
+        // make sure there are at most MAX_ERRORS
+        for i in 0..29 {
+            let message = format!("{} times verboten", i + 1);
+            let e = Error::new(ErrorKind::PermissionDenied, message);
+            expected.last_errors.push_back(format!("{:?}", e));
+            add_audio_transcriptions_error(e).await;
+        }
+
+        assert_eq!(expected.last_errors.len(), MAX_ERRORS);
+
+        {
+            let status = get_audio_transcriptions_status().read().await;
+            assert_eq!(*status, expected);
+        }
+
+        for i in 0..10 {
+            let message = format!("{} times more verboten", i + 1);
+            let e = Error::new(ErrorKind::PermissionDenied, message);
+            expected.last_errors.pop_front();
+            expected.last_errors.push_back(format!("{:?}", e));
+            add_audio_transcriptions_error(e).await;
+        }
+
+        assert_eq!(expected.last_errors.len(), MAX_ERRORS);
+
+        {
+            // since we don't know the exact order in which tokio runs the tasks
+            // the order of errors in the deques is random.
+            // therefore, we sort them before asserting equality.
+            let status = get_audio_transcriptions_status().read().await;
+            let mut v1 = Vec::from(status.last_errors.clone());
+            let mut v2 = Vec::from(expected.last_errors.clone());
+            assert_eq!(v1.sort(), v2.sort());
+        }
+
+        // axum router
+        let router =
+            Router::new().route("/v1/audio/transcriptions/status", get(audio_transcriptions_status));
+
+        let server = TestServer::new(router).expect("cannot instantiate TestServer");
+
+        let response = server.get("/v1/audio/transcriptions/status").await;
+
+        response.assert_status_ok();
+        assert!(response.text().len() > 0);
+        assert_eq!(response.json::<AIStatus>().active_model, "unknown");
+
+        let model = "shes-a-model-and-shes-looking-good".to_string();
+        set_audio_transcriptions_active_model(&model).await;
+
+        let response = server.get("/v1/audio/transcriptions/status").await;
 
         response.assert_status_ok();
         assert!(response.text().len() > 0);
