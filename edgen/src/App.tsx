7b83ba48ef177674a9ee50edbad3193b0a16e861
Miguel Lourenco <mdsllc603@gmail.com>
2024-02-07 02:01:35 +0000

    gui now has a button that links to edgen chat and has some text so user gets some context to whats happening

--- edgen/src/App.tsx
+++ edgen/src/App.tsx
@@ -7,13 +7,20 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Welcome to Edgen!</h1>
-
       <div>
         <img src={edgenLogo} className="logo edgen" alt="Edgen logo" />
       </div>
 
-      <p>GUI coming soon</p>
+      <p style={{paddingTop: "2rem", fontSize:"2rem"}}>Edgen is now running, go to</p>
+
+      <div style={{width: "100%", paddingTop: "2rem"}}>
+        <button onClick={() => window.location.assign("https://chat.edgen.co")} style={{padding:"1.1rem", fontWeight:"bolder", fontSize:"1.5rem", backgroundColor:"#fdd000", color:"#333"}}>
+          Edgen Chat
+        </button>
+      </div>
+
+      <p style={{paddingTop: "2rem", fontSize:"2rem"}}>and start chatting!!</p>
+
     </div>
   );
 }
