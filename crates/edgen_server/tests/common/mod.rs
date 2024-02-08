470ac763ddd7867aa5f14270defcfb5b7f3cd7d5
Tobias Schoofs <tobias.schoofs@gmx.net>
2024-02-08 19:29:42 +0000

    [chore/issue45] test ai endpoints, observe download

--- crates/edgen_server/tests/common/mod.rs
+++ crates/edgen_server/tests/common/mod.rs
@@ -2,17 +2,50 @@ use std::fs;
 use std::io;
 use std::panic;
 use std::path::Path;
+use std::str;
 use std::thread;
+use std::time::{Duration, Instant};
 
 use copy_dir::copy_dir;
+use reqwest::blocking;
+use serde_yaml;
 
 use edgen_core::settings;
+use edgen_core::settings::SettingsParams;
 use edgen_server::cli;
 use edgen_server::start;
+use edgen_server::status;
 
-pub fn test_message(msg: &str) {
-    println!("=== Test {}", msg);
-}
+pub const SMALL_LLM_NAME: &str = "phi-2.Q2_K.gguf";
+pub const SMALL_LLM_REPO: &str = "TheBloke/phi-2-GGUF";
+
+pub const BASE_URL: &str = "http://localhost:33322/v1";
+pub const CHAT_URL: &str = "/chat";
+pub const COMPLETIONS_URL: &str = "/completions";
+pub const AUDIO_URL: &str = "/audio";
+pub const TRANSCRIPTIONS_URL: &str = "/transcriptions";
+pub const STATUS_URL: &str = "/status";
+pub const MISC_URL: &str = "/misc";
+pub const VERSION_URL: &str = "/version";
+
+pub const CHAT_COMPLETIONS_BODY: &str = r#"
+    {
+        "model": "this is not a model",
+        "messages": [
+          {
+            "role": "system",
+            "content": "You are a helpful assistant."
+          },
+          {
+            "role": "user",
+            "content": "what is the result of 1 + 2?"
+          }
+        ],
+        "stream": true
+  }
+"#;
+
+pub const AUDIO_TRANSCRIPTIONS_BODY: &str = "";
 
 pub fn with_save_env<F>(f: F)
 where
@@ -24,6 +57,7 @@ where
 
     backup_env().unwrap();
 
+    println!("==============");
     println!("STARTING TESTS");
     println!("==============");
 
@@ -31,6 +65,7 @@ where
 
     println!("===========");
     println!("TESTS READY");
+    println!("===========");
 
     let _ = match restore_env() {
         Ok(_) => (),
@@ -63,9 +98,6 @@ where
     thread::sleep(std::time::Duration::from_secs(1));
 
     f();
-
-    // give the server time to stop
-    thread::sleep(std::time::Duration::from_secs(3));
 }
 
 pub fn with_save_edgen<F>(f: F)
@@ -75,6 +107,114 @@ where
     with_save_env(|| {
         with_edgen(f);
     });
+}
+
+pub fn test_message(msg: &str) {
+    println!("=== Test {}", msg);
+}
+
+pub fn make_url(v: &[&str]) -> String {
+    let mut s = "".to_string();
+    for e in v {
+        s += e;
+    }
+    s
+}
+
+#[derive(Debug)]
+pub enum ConfigError {
+    IOError(io::Error),
+    YamlError(serde_yaml::Error),
+    Utf8Error(str::Utf8Error),
+}
+
+impl From<io::Error> for ConfigError {
+    fn from(e: io::Error) -> Self {
+        ConfigError::IOError(e)
+    }
+}
+
+impl From<str::Utf8Error> for ConfigError {
+    fn from(e: str::Utf8Error) -> Self {
+        ConfigError::Utf8Error(e)
+    }
+}
+
+impl From<serde_yaml::Error> for ConfigError {
+    fn from(e: serde_yaml::Error) -> Self {
+        ConfigError::YamlError(e)
+    }
+}
+
+pub fn get_config() -> Result<SettingsParams, ConfigError> {
+    let path = settings::get_config_file_path();
+    let buf = fs::read(path)?;
+    let yaml = str::from_utf8(&buf)?;
+    Ok(serde_yaml::from_str(yaml)?)
+}
+
+pub fn write_config(config: &SettingsParams) -> Result<(), ConfigError> {
+    let path = settings::get_config_file_path();
+    let yaml = serde_yaml::to_string(config)?;
+    let buf = yaml.as_bytes().to_vec();
+    fs::write(path, buf)?;
+    Ok(())
+}
+
+pub fn spawn_request(endpoint: String, body: String) -> thread::JoinHandle<bool> {
+    thread::spawn(move || {
+        println!("requesting {}", endpoint);
+        match blocking::Client::new()
+            .post(&endpoint)
+            .header("Content-Type", "application/json")
+            .body(body)
+            .timeout(Duration::from_secs(180))
+            .send()
+        {
+            Err(e) => {
+                eprintln!("cannot connect: {:?}", e);
+                false
+            }
+            Ok(v) => {
+                println!("Got {:?}", v);
+                true
+            }
+        }
+    })
+}
+
+pub fn observe_progress(endpoint: &str) -> bool {
+
+    println!("requesting status of {}", endpoint);
+
+    let mut stat: status::AIStatus = blocking::get(endpoint).unwrap().json().unwrap();
+
+    let mut tp = Instant::now();
+    while !stat.download_ongoing {
+        thread::sleep(Duration::from_millis(100));
+        stat = blocking::get(endpoint).unwrap().json().unwrap();
+
+        if Instant::now().duration_since(tp) > Duration::from_secs(60) {
+            break;
+        }
+    }
+
+    assert!(stat.download_ongoing);
+
+    let mut last_p = 0;
+    while stat.download_ongoing {
+        let p = stat.download_progress;
+        if p > last_p {
+            last_p = p;
+            tp = Instant::now();
+        } else {
+            assert!(Instant::now().duration_since(tp) < Duration::from_secs(60));
+        }
+        thread::sleep(Duration::from_millis(30));
+        stat = blocking::get(endpoint).unwrap().json().unwrap();
+    }
+    assert_eq!(stat.download_progress, 100);
+    true
 }
 
 const BACKUP_DIR: &str = "env_backup";
--- crates/edgen_server/tests/settings_tests.rs
+++ crates/edgen_server/tests/settings_tests.rs
@@ -1,6 +1,7 @@
 use reqwest::blocking;
 
 use edgen_core::settings;
+use edgen_server::status;
 
 mod common;
 
@@ -18,23 +19,44 @@ fn test_battery() {
         connect_to_server_test();
         chat_completions_status_reachable();
         audio_transcriptions_status_reachable();
-    });
+        set_chat_completions_model(common::SMALL_LLM_NAME, common::SMALL_LLM_REPO);
+        // set_audio_transcriptions_model
+        test_ai_endpoint_with_download(Endpoint::ChatCompletions);
+        // test_ai_endpoint_with_download(Endpoint::AudioTranscriptions);
+        // test_ai_endpoint_no_download(Endpoint::ChatCompletions);
+        // test_ai_endpoint_no_download(Endpoint::AudioTranscriptions);
+        // update model dir
+        // test_ai_endpoint_with_download(Endpoint::ChatCompletions);
+        // test_ai_endpoint_with_download(Endpoint::AudioTranscriptions);
+        // test_ai_endpoint_no_download(Endpoint::ChatCompletions);
+        // test_ai_endpoint_no_download(Endpoint::AudioTranscriptions);
+        // remove model dir
+        // test_ai_endpoint_with_download(Endpoint::ChatCompletions);
+        // test_ai_endpoint_with_download(Endpoint::AudioTranscriptions);
+        // test_ai_endpoint_no_download(Endpoint::ChatCompletions);
+        // test_ai_endpoint_no_download(Endpoint::AudioTranscriptions);
+        // config reset
+        // test_ai_endpoint_no_download(Endpoint::ChatCompletions);
+        // test_ai_endpoint_no_download(Endpoint::AudioTranscriptions);
+    })
 }
 
 fn connect_to_server_test() {
     common::test_message("connect to server");
-    assert!(
-        match blocking::get("http://localhost:33322/v1/misc/version") {
-            Err(e) => {
-                eprintln!("cannot connect: {:?}", e);
-                false
-            }
-            Ok(v) => {
-                println!("have: '{}'", v.text().unwrap());
-                true
-            }
+    assert!(match blocking::get(common::make_url(&[
+        common::BASE_URL,
+        common::MISC_URL,
+        common::VERSION_URL
+    ])) {
+        Err(e) => {
+            eprintln!("cannot connect: {:?}", e);
+            false
         }
-    );
+        Ok(v) => {
+            println!("have: '{}'", v.text().unwrap());
+            true
+        }
+    });
 }
 
 fn pass_always() {
@@ -77,32 +99,104 @@ fn data_exists() {
 
 fn chat_completions_status_reachable() {
     common::test_message("chat completions status is reachable");
-    assert!(
-        match blocking::get("http://localhost:33322/v1/chat/completions/status") {
-            Err(e) => {
-                eprintln!("cannot connect: {:?}", e);
-                false
-            }
-            Ok(v) => {
-                println!("have: '{}'", v.text().unwrap());
-                true
-            }
+    assert!(match blocking::get(common::make_url(&[
+        common::BASE_URL,
+        common::CHAT_URL,
+        common::COMPLETIONS_URL,
+        common::STATUS_URL,
+    ])) {
+        Err(e) => {
+            eprintln!("cannot connect: {:?}", e);
+            false
         }
-    );
+        Ok(v) => {
+            println!("have: '{}'", v.text().unwrap());
+            true
+        }
+    });
 }
 
 fn audio_transcriptions_status_reachable() {
     common::test_message("audio transcriptions status is reachable");
-    assert!(
-        match blocking::get("http://localhost:33322/v1/audio/transcriptions/status") {
-            Err(e) => {
-                eprintln!("cannot connect: {:?}", e);
-                false
-            }
-            Ok(v) => {
-                println!("have: '{}'", v.text().unwrap());
-                true
-            }
+    assert!(match blocking::get(common::make_url(&[
+        common::BASE_URL,
+        common::AUDIO_URL,
+        common::TRANSCRIPTIONS_URL,
+        common::STATUS_URL,
+    ])) {
+        Err(e) => {
+            eprintln!("cannot connect: {:?}", e);
+            false
         }
-    );
+        Ok(v) => {
+            println!("have: '{}'", v.text().unwrap());
+            true
+        }
+    });
+}
+
+fn set_chat_completions_model(model_name: &str, model_repo: &str) {
+    common::test_message(&format!("set chat completions model to {}", model_name,));
+
+    let mut config = common::get_config().unwrap();
+    config.chat_completions_model_name = model_name.to_string();
+    config.chat_completions_model_repo = model_repo.to_string();
+    common::write_config(&config).unwrap();
+
+    println!("pausing for 4 secs");
+    std::thread::sleep(std::time::Duration::from_secs(4));
+    let stat: status::AIStatus = blocking::get(common::make_url(&[
+        common::BASE_URL,
+        common::CHAT_URL,
+        common::COMPLETIONS_URL,
+        common::STATUS_URL,
+    ]))
+    .unwrap()
+    .json()
+    .unwrap();
+
+    assert_eq!(stat.active_model, model_name);
+}
+
+enum Endpoint {
+    ChatCompletions,
+    AudioTranscriptions,
+}
+
+fn test_ai_endpoint_with_download(endpoint: Endpoint) {
+    let (ep, stp, body) = match endpoint {
+        Endpoint::ChatCompletions => {
+            common::test_message("chat completions endpoint with download");
+            (
+                common::make_url(&[common::BASE_URL, common::CHAT_URL, common::COMPLETIONS_URL]),
+                common::make_url(&[
+                    common::BASE_URL,
+                    common::CHAT_URL,
+                    common::COMPLETIONS_URL,
+                    common::STATUS_URL,
+                ]),
+                common::CHAT_COMPLETIONS_BODY.to_string(),
+            )
+        }
+        Endpoint::AudioTranscriptions => {
+            common::test_message("audio transcriptions endpoint with progress");
+            (
+                common::make_url(&[
+                    common::BASE_URL,
+                    common::AUDIO_URL,
+                    common::TRANSCRIPTIONS_URL,
+                ]),
+                common::make_url(&[
+                    common::BASE_URL,
+                    common::AUDIO_URL,
+                    common::TRANSCRIPTIONS_URL,
+                    common::STATUS_URL,
+                ]),
+                common::AUDIO_TRANSCRIPTIONS_BODY.to_string(),
+            )
+        }
+    };
+    let handle = common::spawn_request(ep, body);
+    assert!(common::observe_progress(&stp));
+    assert!(handle.join().unwrap());
 }
